0a257c8479a81e924ecffe06d606fd6e09b83857
Priyanka <priyankadombale27@gmail.com>
2022-03-25 09:04:17 +0530

    Added: Welcome Message in Addressbook System

---
+++ src/main/java/com/AddressBook_Workshop/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.AddressBook_Workshop;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println(" Welcome to AddressBook System program..!!");
+
+	}
+
+}
