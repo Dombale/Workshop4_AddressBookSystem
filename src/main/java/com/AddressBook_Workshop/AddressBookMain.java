bcb036d47e26078fd23136253329a7d225ef5977
Priyanka <priyankadombale27@gmail.com>
2022-03-25 10:39:02 +0530

    Added: Add all contact details

--- src/main/java/com/AddressBook_Workshop/AddressBookMain.java
+++ src/main/java/com/AddressBook_Workshop/AddressBookMain.java
@@ -1,10 +1,22 @@
 package com.AddressBook_Workshop;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @Purpose : List And AddContact function
+ * @Param: Contacts
+ * @return : ContactList
+ */
 public class AddressBookMain {
+	List<Contacts> contactList = new ArrayList<Contacts>();
 
-	public static void main(String[] args) {
-		System.out.println(" Welcome to AddressBook System program..!!");
+	public void addContact(Contacts contacts) {
+		contactList.add(contacts);
+	}
 
+	public List<Contacts> getcontactList() {
+		return contactList;
 	}
 
 }
